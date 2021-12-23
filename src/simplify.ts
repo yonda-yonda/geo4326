c194e9a9acf9db76a89cdea7200b44c1c67a7ba7
yonda-yonda <minerva.yooda@gmail.com>
2021-12-24 02:09:03 +0900

    add vw

--- src/simplify.ts
+++ src/simplify.ts
@@ -81,12 +81,12 @@ export function rdp(
     const threshold = options.area ? calcArea(linearRing) * options.threshold : options.threshold;
     const first = 0;
     const last = linearRing.length - 1;
-    let memory = -Infinity;
+    let score = -Infinity;
     let midpoint = -1;
     for (let i = first + 1; i < last; i++) {
         const d2 = _dist2(linearRing[first], linearRing[i]);
-        if (d2 > memory) {
-            memory = d2;
+        if (d2 > score) {
+            score = d2;
             midpoint = i;
         }
     }
@@ -101,6 +101,95 @@ export function rdp(
         candidates = candidates.slice(0, options.limit - 3);
     }
     const candidateIndexes = [first, last, midpoint, ...candidates.map((v) => v.index)];
+    return linearRing.filter((_, i) => {
+        return candidateIndexes.includes(i)
+    })
+}
+
+
+export interface vwOptions {
+    rate?: boolean;
+    threshold?: number;
+    limit?: number;
+}
+
+export function vw(
+    linearRing: Points,
+    userOptions?: vwOptions
+): Points {
+    /*
+    Modified Visvalingam–Whyatt
+  
+    Original Algorithm
+    https://en.wikipedia.org/wiki/Visvalingam%E2%80%93Whyatt_algorithm
+    */
+    validLinearRing(linearRing);
+    const options = Object.assign(
+        {
+            rate: false,
+            threshold: 0.001,
+            limit: Infinity
+        },
+        userOptions
+    );
+
+    if (options.rate) {
+        if (options.threshold < 0 || options.threshold >= 1)
+            throw new InvalidSimplifyError("when rate is true, threshold must be between 0 and 1.");
+    } else {
+        if (options.threshold < 0)
+            throw new InvalidSimplifyError("threshold must be positive number.");
+    }
+    if (options.limit < 4)
+        throw new InvalidSimplifyError("limit must be larger than 3.");
+
+    const threshold = options.rate ? calcArea(linearRing) * options.threshold : options.threshold;
+
+    const candidates = linearRing.map((point, i) => {
+        if (i === 0 || i === linearRing.length - 1)
+            return {
+                score: Infinity,
+                index: i
+            }
+
+        return {
+            score: calcArea([linearRing[i - 1], point, linearRing[i + 1], linearRing[i - 1]]),
+            index: i
+        }
+    })
+
+    while (candidates.length > 4) {
+        let score = Infinity;
+        let eliminate = -1;
+        for (let i = 1; i < candidates.length - 1; i++) {
+            if (candidates[i].score < score) {
+                score = candidates[i].score;
+                eliminate = i;
+            }
+        }
+        if (candidates.length > options.limit || score < threshold) {
+            if (eliminate - 2 >= 0) {
+                const p1 = candidates[eliminate - 2].index;
+                const p2 = candidates[eliminate - 1].index;
+                const p3 = candidates[eliminate + 1].index;
+                candidates[eliminate - 1].score = calcArea([
+                    linearRing[p1], linearRing[p2], linearRing[p3], linearRing[p1]
+                ])
+            }
+            if (eliminate + 2 <= candidates.length - 1) {
+                const p1 = candidates[eliminate - 1].index;
+                const p2 = candidates[eliminate + 1].index;
+                const p3 = candidates[eliminate + 2].index;
+                candidates[eliminate + 1].score = calcArea([
+                    linearRing[p1], linearRing[p2], linearRing[p3], linearRing[p1]
+                ])
+            }
+            candidates.splice(eliminate, 1)
+        } else {
+            break;
+        }
+    }
+    const candidateIndexes = [...candidates.map((v) => v.index)];
     return linearRing.filter((_, i) => {
         return candidateIndexes.includes(i)
     })
