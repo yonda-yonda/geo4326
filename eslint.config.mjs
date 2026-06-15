7e7f4fb38f90e057d5c895aedcdc23333484b8ae
yonda-yonda <minerva.yooda@gmail.com>
2026-06-15 16:28:56 +0900

    add lint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,15 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  {
+    ignores: [
+      "dist/",
+      "build/",
+      "node_modules/",
+      "script/",
+      "src/_generated/*",
+    ],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-    ignores: ["src/_generated/*"],
-  },
 );
